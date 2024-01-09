2eb73bee037f57e3aa92107d3a242ae786618dc7
smithclay <clay@lightstep.com>
2024-01-09 09:59:48 -0800

    add some tests

--- collector/components/osqueryreceiver/config.go
+++ collector/components/osqueryreceiver/config.go
@@ -1,6 +1,8 @@
 package osqueryreceiver
 
 import (
+	"errors"
+
 	"go.opentelemetry.io/collector/receiver/scraperhelper"
 )
 
@@ -8,4 +10,11 @@ type Config struct {
 	scraperhelper.ScraperControllerSettings `mapstructure:",squash"`
 	ExtensionsSocket                        string   `mapstructure:"extensions_socket"`
 	Queries                                 []string `mapstructure:"queries"`
+}
+
+func (c Config) Validate() error {
+	if len(c.Queries) == 0 {
+		return errors.New("queries cannot be empty")
+	}
+	return nil
 }
