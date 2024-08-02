86b65a97937781807e18292d6e1fa4d20278afb4
smithclay <clay@lightstep.com>
2024-08-02 14:27:36 -0700

    switch from carbon exporter to prometheus exporter

--- sensu-plugin/bin/otel-metric-collector.rb
+++ sensu-plugin/bin/otel-metric-collector.rb
@@ -1,17 +1,6 @@
-#!/usr/bin/env ruby
-
-require 'sensu-plugin/check/cli'
-require 'json'
-require 'socket'
-require 'timeout'
-
-DEFAULT_PORT = 2003
-DEFAULT_HOST = '0.0.0.0'
-DEFAULT_TIMEOUT = 30
-
 #
-# Collects OpenTelemetry metrics received from a carbon exporter
-# listening on a TCP port.
+# Collects OpenTelemetry metrics received from a Prometheus exporter
+# and converts to Carbon format for Sensu to process.
 #
 class CollectOTelMetrics < Sensu::Plugin::Check::CLI
   option :timeout,
@@ -27,41 +16,47 @@ class CollectOTelMetrics < Sensu::Plugin::Check::CLI
     long: '--port PORT',
     default: DEFAULT_PORT
 
-  def start_server
-    server = TCPServer.new(config[:host], config[:port])
-    #puts "Server started on #{HOST}:#{PORT}"
+  # Convert Prometheus format to Carbon format
+  def prometheus_to_carbon(prometheus_metric)
+    metric, value, timestamp = prometheus_metric.split(' ')
 
-    begin
-      Timeout.timeout(config[:timeout]) do
-        loop do
-          client = server.accept
-          Thread.new(client) do |client_connection|
-            handle_client(client_connection)
-          end
-        end
-      end
-    rescue Timeout::Error
-      #puts "Server timeout reached. Shutting down..."
-    ensure
-      # TODO: handle close nicely to avoid
-      # WARNING: Check did not exit! You should call an exit code method.
-      server.close
-    end
+    metric_name, labels_string = metric.split('{')
+    labels_string.chomp!('}') if labels_string
+
+    labels = labels_string ? labels_string.split(',').map { |label| label.split('=') }.to_h : {}
+
+    carbon_metric_name = metric_name.gsub('_', '.').sub('.', '-')
+    carbon_labels = labels.map { |k, v| "#{k}=#{v.gsub('"', '')}" }.join(';')
+    carbon_metric_name += ";#{carbon_labels}" unless carbon_labels.empty?
+
+    timestamp = (timestamp.to_i / 1000).to_s
+
+    carbon_metric = "#{carbon_metric_name} #{value} #{timestamp}"
+
+    carbon_metric
   end
 
-  def handle_client(client)
-    loop do
-      line = client.gets
-      break if line.nil? || line.chomp.empty?
+  # Fetches metric from prometheus endpoint passed in as CLI options
+  def fetch_and_process_metrics()
+    uri = URI("http://#{config[:host]}:#{config[:port]}#{DEFAULT_PATH}")
+    response = Net::HTTP.get_response(uri)
 
-      puts line.chomp
-      client.puts line
+    if response.is_a?(Net::HTTPSuccess)
+      response.body.each_line do |line|
+        line.strip!
+        next if line.empty? || line.start_with?('#')
+
+        carbon_metric = prometheus_to_carbon(line)
+        puts carbon_metric
+      end
+    else
+      critical "Failed to fetch metrics: #{response.message}"
+      exit 1
     end
-
-    client.close
   end
 
   def run
-    start_server
+    fetch_and_process_metrics
+    exit 0
   end
 end
